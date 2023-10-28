1e2adbb4eec8bc0abf3a34125bb842f081064589
Aryan@123-rgb <srivatavaaryanalc76@gmail.com>
2023-10-29 01:14:01 +0530

    initialized a nextjs app with shadcn ui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-2xl text-red-500">Hello World</p>
+      <Button variant={"ghost"}>Click Me</Button>
+    </div>
+  );
+}
